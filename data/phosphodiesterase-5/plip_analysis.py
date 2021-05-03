d608e71c3f24712cfdc754c669e9505199075c0c
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-05-03 08:15:11 +0200

    add PDE5 results

---
+++ data/phosphodiesterase-5/plip_analysis.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# PLIP ANALYSIS
+# 2021 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+import os
+import pandas as pd
+from shutil import copyfile
+
+if __name__ == '__main__':
+
+    # copy most recent version of PLIPAnalyzer to directory
+    copyfile("../../scripts/python/PLIPAnalyzer.py", "PLIPAnalyzer.py")
+
+    # import PLIPAnalyzer
+    from PLIPAnalyzer import PLIPAnalyzer as pa
+
+    # analysis
+    df = pd.read_csv("data_train.csv")
+    PDB_ENTRIES = list(df["PDB_ENTRY"])
+    print("First 5 PDB entries:")
+    print(PDB_ENTRIES[:5])
+    result = pa(PDB_ENTRIES, path = "structures")
+    r = result.save("results/phosphodiesterase-5")
+    print("Result saved in:")
+    print(r)
+    r = result.to_csv("results/phosphodiesterase-5_freq.csv")
+    result.plot("Phosphodiesterase-5", filename = "results/phosphodiesterase-5.jpg")
+    result.plot("Phosphodiesterase-5", filename = "results/phosphodiesterase-5.png")
+
+    # clean up
+    os.remove("PLIPAnalyzer.py")
