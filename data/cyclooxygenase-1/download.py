d19559c41497d01a095a9ebc6f265016dcad04e6
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-05-03 09:59:59 +0200

    add COX1 results

---
+++ data/cyclooxygenase-1/download.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# PDB DOWNLOAD SCRIPT
+# 2021 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+import urllib.request as ur
+
+def download_pdb_files(filename, output_path = "structures"):
+
+    pdb_link = "https://files.rcsb.org/download/"
+
+    with open(filename, "r") as f:
+        content = f.read()
+        f.close()
+
+    fields = content.split(";")
+    tmp = fields[5]
+    if tmp.split(":")[0].strip() == "PDB_ENTRIES":
+        PDB_ENTRIES = tmp.split(":")[1].split(",")
+    else:
+        print("Wrong field selected!")
+        print(tmp.split(":")[0])
+        return 1
+
+    for entry in PDB_ENTRIES:
+        output_name = output_path + "/" + entry.strip() + ".pdb"
+        link = pdb_link + entry.strip() + ".pdb"
+        ur.urlretrieve(link, output_name)
+
+    return 0
+
+if __name__ == "__main__":
+    r = download_pdb_files("PDB2.txt")
+    print(r)
