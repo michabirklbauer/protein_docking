da7999e7234f8910ed56a6421ddbc5ece8f826d0
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-05-29 21:22:08 +0200

    bump PTP1B results to newer version

--- data/protein-tyrosine_phosphatase_1B/plip_analysis.py
+++ data/protein-tyrosine_phosphatase_1B/plip_analysis.py
@@ -22,13 +22,21 @@ if __name__ == '__main__':
     PDB_ENTRIES = list(df["PDB_ENTRY"])
     print("First 5 PDB entries:")
     print(PDB_ENTRIES[:5])
-    result = pa(PDB_ENTRIES, path = "structures")
+    result = pa(PDB_ENTRIES, path = "structures", normalize = False)
     r = result.save("results/protein-tyrosine_phosphatase_1B")
     print("Result saved in:")
     print(r)
     r = result.to_csv("results/protein-tyrosine_phosphatase_1B_freq.csv")
     result.plot("Protein-Tyrosine Phosphatase 1B", filename = "results/protein-tyrosine_phosphatase_1B.jpg")
     result.plot("Protein-Tyrosine Phosphatase 1B", filename = "results/protein-tyrosine_phosphatase_1B.png")
+    # normalized results
+    result = pa(PDB_ENTRIES, path = "structures", normalize = True)
+    r = result.save("results/protein-tyrosine_phosphatase_1B_normalized")
+    print("Result saved in:")
+    print(r)
+    r = result.to_csv("results/protein-tyrosine_phosphatase_1B_normalized_freq.csv")
+    result.plot("Protein-Tyrosine Phosphatase 1B", filename = "results/protein-tyrosine_phosphatase_1B_normalized.jpg")
+    result.plot("Protein-Tyrosine Phosphatase 1B", filename = "results/protein-tyrosine_phosphatase_1B_normalized.png")
 
     # clean up
     os.remove("PLIPAnalyzer.py")
