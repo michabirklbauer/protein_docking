1eef4e583e809c644d59d490bd77a241df35e7be
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-06-07 21:18:53 +0200

    bump COX1 results to newer version

--- data/cyclooxygenase-1/plip_analysis.py
+++ data/cyclooxygenase-1/plip_analysis.py
@@ -22,13 +22,21 @@ if __name__ == '__main__':
     PDB_ENTRIES = list(df["PDB_ENTRY"])
     print("First 5 PDB entries:")
     print(PDB_ENTRIES[:5])
-    result = pa(PDB_ENTRIES, path = "structures")
+    result = pa(PDB_ENTRIES, path = "structures", normalize = False)
     r = result.save("results/cyclooxygenase-1")
     print("Result saved in:")
     print(r)
     r = result.to_csv("results/cyclooxygenase-1_freq.csv")
     result.plot("Cyclooxygenase-1", filename = "results/cyclooxygenase-1.jpg")
     result.plot("Cyclooxygenase-1", filename = "results/cyclooxygenase-1.png")
+    # normalized analysis
+    result = pa(PDB_ENTRIES, path = "structures", normalize = True)
+    r = result.save("results/cyclooxygenase-1_normalized")
+    print("Result saved in:")
+    print(r)
+    r = result.to_csv("results/cyclooxygenase-1_normalized_freq.csv")
+    result.plot("Cyclooxygenase-1", filename = "results/cyclooxygenase-1_normalized.jpg")
+    result.plot("Cyclooxygenase-1", filename = "results/cyclooxygenase-1_normalized.png")
 
     # clean up
     os.remove("PLIPAnalyzer.py")
