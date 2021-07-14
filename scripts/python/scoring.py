ad6db8ba75ac5b675397e26cdf4b6d95adf6a16a
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-07-14 15:52:39 +0200

    update scoring functions and re-run SEH notebook

--- scripts/python/scoring.py
+++ scripts/python/scoring.py
@@ -14,6 +14,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, roc_curve, auc
 
 # get relevant features from a dataframe of features (threshold based)
 def get_relevant_features(features,
@@ -149,10 +150,14 @@ def test_cutoff(labels,
 
 # get quality metrics for a dataframe with score and labels
 def get_metrics(dataframe,
-                cutoff):
+                cutoff,
+                pretty_print = False):
 
     """
     -- DESCRIPTION --
+    ACC: Accuracy
+    FPR: False Positive Rate
+    AUC: Area under the ROC curve
     Ya: Yield of actives
     EF: Enrichment Factor
     REF: Relative Enrichment Factor
@@ -162,32 +167,88 @@ def get_metrics(dataframe,
                  Ns = n
                  n = A
                  ns = TP
+    CM: Confusion Matrix
+    ROC: ROC curve
     """
 
+    scores = []
+    y_true, y_predicted = [], []
     TP, n, A, N = 0, 0, 0, 0
+    FP, Neg = 0, 0
+    TN = 0
 
     for index, row in dataframe.iterrows():
+        y_true.append(1 if row["LABEL"] == "active" else 0)
+        y_predicted.append(1 if row["SCORE"] >= cutoff else 0)
+        scores.append(row["SCORE"])
         if row["SCORE"] >= cutoff:
             n = n + 1
         if (row["SCORE"] >= cutoff) and (row["LABEL"] == "active"):
             TP = TP + 1
+        if (row["SCORE"] >= cutoff) and (row["LABEL"] == "inactive"):
+            FP = FP + 1
+        if (row["SCORE"] < cutoff) and (row["LABEL"] == "inactive"):
+            TN = TN + 1
         if row["LABEL"] == "active":
             A = A + 1
+        else:
+            Neg = Neg + 1
         N = N + 1
 
-    return {"Ya": TP / n, "EF": (TP / n) / (A / N), "REF": (100 * TP) / min(n, A)}
+    scores_normalized = [i / max(scores) for i in scores]
+    # see https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html#sklearn.metrics.roc_curve
+    roc_fpr, roc_tpr, roc_thresholds = roc_curve(y_true, scores_normalized)
+    # see https://scikit-learn.org/stable/modules/generated/sklearn.metrics.auc.html#sklearn.metrics.auc
+    roc_auc = auc(roc_fpr, roc_tpr)
+
+    if pretty_print:
+        return {"ACC": (TP + TN) / N, "FPR": FP / Neg, "AUC": roc_auc, "Ya": TP / n,
+                "EF": (TP / n) / (A / N), "REF": (100 * TP) / min(n, A)}
+    else:
+        return {"ACC": (TP + TN) / N, "FPR": FP / Neg, "AUC": roc_auc, "Ya": TP / n,
+                "EF": (TP / n) / (A / N), "REF": (100 * TP) / min(n, A),
+                "CM": confusion_matrix(y_true, y_predicted),
+                "ROC": {"fpr": roc_fpr, "tpr": roc_tpr}}
+
+# plot ROC curve
+def plot_ROC_curve(fpr,
+                   tpr,
+                   title = "Receiver Operating Characteristic curve",
+                   filename = None,
+                   width = 10,
+                   height = 10):
+
+    # see https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html#sphx-glr-auto-examples-model-selection-plot-roc-py
+    fig = plt.figure(figsize = (width, height))
+    plt.plot(fpr, tpr, color = "#ff7f0e", label = "ROC curve (area = %0.2f)" % auc(fpr, tpr))
+    plt.plot([0, 1], [0, 1], color = "#1f77b4", linestyle = "--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc = "lower right")
+    if filename is not None:
+            fig.savefig(filename, bbox_inches = "tight", dpi = 150)
+    plt.show()
+
+    return fig
 
 # plot confusion matrix
 def plot_confusion_matrix(cm,
                           classes,
                           normalize = False,
                           title = "Confusion matrix",
+                          filename = None,
+                          width = 10,
+                          height = 10,
                           cmap = plt.cm.Blues):
 
     """
     -- DESCRIPTION --
     """
 
+    fig = plt.figure(figsize = (width, height))
     plt.imshow(cm, interpolation="nearest", cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -209,5 +270,8 @@ def plot_confusion_matrix(cm,
     plt.ylim(len(cm) - 0.5, -0.5)
     plt.ylabel("True label")
     plt.xlabel("Predicted label")
+    if filename is not None:
+            fig.savefig(filename, bbox_inches = "tight", dpi = 150)
+    plt.show()
 
     return cm
