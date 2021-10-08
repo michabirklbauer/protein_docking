5daf2970d2ddacfb21a648b97dc879bc60f04927
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-10-08 13:58:53 +0200

    update script for PIA 1.0.0

--- data/acetylcholinesterase/pia_analysis.py
+++ data/acetylcholinesterase/pia_analysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# PLIP ANALYSIS
+# PIA ANALYSIS
 # 2021 (c) Micha Johannes Birklbauer
 # https://github.com/michabirklbauer/
 # micha.birklbauer@gmail.com
@@ -11,11 +11,11 @@ from shutil import copyfile
 
 if __name__ == '__main__':
 
-    # copy most recent version of PLIPAnalyzer to directory
-    copyfile("../../scripts/python/PLIPAnalyzer.py", "PLIPAnalyzer.py")
+    # copy most recent version of PIA to directory
+    copyfile("../../scripts/python/PIA.py", "PIA.py")
 
-    # import PLIPAnalyzer
-    from PLIPAnalyzer import PLIPAnalyzer as pa
+    # import PIA
+    from PIA import PIA as pa
 
     # analysis
     df = pd.read_csv("data_train.csv")
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     result.plot("Acetylcholinesterase", filename = "results/acetylcholinesterase_normalized.png")
 
     # clean up
-    os.remove("PLIPAnalyzer.py")
+    os.remove("PIA.py")
