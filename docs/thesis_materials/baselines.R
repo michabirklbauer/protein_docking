33459334ce7e40ce1c0196a9e60f7f1825a719a5
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-10-09 01:57:00 +0200

    add thesis materials

---
+++ docs/thesis_materials/baselines.R
@@ -0,0 +1,25 @@
+# ACHE
+ache_actives = 427 + 104 + 133
+ache_total = 764 + 192 + 239
+ache_inactives = ache_total - ache_actives
+baseline = ache_actives / ache_total
+# COX1
+cox_actives = 223 + 60 + 74
+cox_total = 790 + 198 + 248
+cox_inactives = cox_total - cox_actives
+baseline = cox_inactives / cox_total
+# DPP4
+dpp_actives = 654 + 179 + 210
+dpp_total = 1291 + 323 + 404
+dpp_inactives = dpp_total - dpp_actives
+baseline = dpp_actives / dpp_total
+# MAOB
+maob_actives = 113 + 24 + 31
+maob_total = 282 + 71 + 89
+maob_inactives = maob_total - maob_actives
+baseline = maob_inactives / maob_total
+# SEH
+seh_actives = 39 + 8 + 11
+seh_total = 150 + 38 +48
+seh_inactives = seh_total - seh_actives
+baseline = seh_inactives / seh_total
